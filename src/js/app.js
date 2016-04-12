852a7644d93b5945ff766451e34c7b97c4e54738
Peter Jihoon Kim <raingrove@gmail.com>
2016-04-12 05:23:22 +0000

    designed homepage

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 import 'bootstrap.min';
 
-$(function() {});
+$('.npm-cmd').click(function() {
+  $(this).select();
+});
+
+$('.copy-npm-cmd-btn').click(function(e) {
+  e.preventDefault();
+  $('.npm-cmd').select();
+  document.execCommand('copy');
+}).tooltip({
+  container: 'body',
+  trigger: 'hover'
+});
+
+$('.get-started-btn').click(function() {
+  location.hash = '';
+  $('html, body').animate({
+    scrollTop: $('#get-started').offset().top
+  }, {
+    duration: 300,
+    complete() {
+      location.hash = 'get-started';
+    }
+  });
+}).tooltip({
+  trigger: 'hover'
+});
